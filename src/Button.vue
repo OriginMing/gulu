c0787e3a204c32fbeec208ce2dc87e807c44c868
miilio <2368102002@qq.com>
2021-07-06 09:27:54 +0800

    "button单页面组件封装parcel打包完成"

---
+++ src/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button class="g-button">你好</button>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped lang="scss">
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    --border-color:#eee;
+  }
+  #app{
+    margin: 20px;
+  }
+  :root{
+    --font-size:14px;
+    --button-height:32px;
+    --button-bg:white;
+    --button-active-bg:#eee;
+    --border-radius:4px;
+    --color:#333;
+    --border-color:#999;
+    --border-color-hover:#666 ;
+  }
+  .g-button{
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    border-radius: var(--border-radius);
+    &:hover{
+      border-color: var(--border-color-hover);
+    }
+    &:active{
+      background: var(--button-active-bg);
+    }
+    &:focus{
+      outline: none;
+    }
+  }
+</style>
