3a156431d452d53d37de80574b696ba3896e0cd4
miilio <2368102002@qq.com>
2021-07-08 13:54:11 +0800

    "app.js进行单元测试"

--- src/app.js
+++ src/app.js
@@ -13,3 +13,24 @@ new Vue({
     isLoading3:true,
   }
 })
+
+import chai from 'chai';
+const exp = chai.expect;
+{
+  //将Button对象转为函数
+  let  Constructor = Vue.extend(Button);
+  //创建Button的实例//给Button实例传参
+  let constructor = new Constructor({
+    propsData:{
+      icon:'set'
+    }
+  });
+  //将Button挂载到页面上
+  constructor.$mount('#test');
+  //
+  let docu =  constructor.$el.querySelector('use');
+  console.log(docu.getAttribute('xlink:href'))
+  let attribute = docu.getAttribute('xlink:href')
+  exp(attribute).to.eq('#icon-set');
+
+}
